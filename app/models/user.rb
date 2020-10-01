480e2299c869f55a851f09716ea0cf2ef6e5fced
hikasa_akito <wetsand.wfs@gmail.com>
2020-10-02 08:47:32 +0900

    メール認証以外の機能を確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable , :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :trackable, :confirmable
+         :trackable
 end
