32e8dcf13762544f01c4b5dc925b18aa8d803c20
hikasa_akito <wetsand.wfs@gmail.com>
2020-10-18 12:41:43 +0900

    helper

--- app/helpers/webpack_helper.rb
+++ app/helpers/webpack_helper.rb
@@ -1,13 +1,51 @@
 # frozen_string_literal: true
 
 module WebpackHelper
-  def webpack_asset_path(path)
-    # webpack-dev-server を参照
-    return "http://localhost:8080/#{path}" if Rails.env.development?
+  # Creates a script tag that references the named pack file, as compiled by webpack per the entries list
+  # in package/environments/base.js. By default, this list is auto-generated to match everything in
+  # app/javascript/packs/*.js. In production mode, the digested reference is automatically looked up.
+  #
+  # Example:
+  #
+  #   <%= javascript_pack_tag 'calendar' %> # =>
+  #   <script src="/packs/calendar-1016838bab065ae1e314.js" ></script>
+  def javascript_pack_tag(*names, **options)
+    javascript_include_tag(*sources_from_manifest_entries(names, type: :javascript), **options)
+  end
 
-    host = Rails.application.config.action_controller.asset_host
-    manifest = Rails.application.config.assets.webpack_manifest
-    path = manifest[path] if manifest && manifest[path].present?
-    "#{host}/assets/#{path}"
+  # Creates a link tag that references the named pack file, as compiled by webpack per the entries list
+  # in package/environments/base.js. By default, this list is auto-generated to match everything in
+  # app/javascript/packs/*.js. In production mode, the digested reference is automatically looked up.
+  #
+  # Note: If the development server is running and hot module replacement is active, this will return nothing.
+  # In that setup you need to configure your styles to be inlined in your JavaScript for hot reloading.
+  #
+  # Examples:
+  #
+  #   # When extract_css is false in webpacker.yml:
+  #   <%= stylesheet_pack_tag 'calendar' %> # =>
+  #   nil
+  #
+  #   # When extract_css is true in webpacker.yml:
+  #   <%= stylesheet_pack_tag 'calendar' %> # =>
+  #   <link rel="stylesheet" media="screen" href="/packs/calendar-1016838bab065ae1e122.css" />
+  def stylesheet_pack_tag(*names, **options)
+    stylesheet_link_tag(*sources_from_manifest_entries(names, type: :stylesheet), **options)
+  end
+
+  private
+
+  # def webpack_asset_path(path)
+  #   # webpack-dev-server を参照
+  #   return "http://localhost:8080/#{path}" if Rails.env.development?
+
+  #   host = Rails.application.config.action_controller.asset_host
+  #   manifest = Rails.application.config.assets.webpack_manifest
+  #   path = manifest[path] if manifest && manifest[path].present?
+  #   "#{host}/assets/#{path}"
+  # end
+
+  def sources_from_manifest_entries(names, type:)
+    names.map { |name| Webpack.manifest.lookup!(name, type: type) }.flatten
   end
 end
