dffa77663a189667806e9a4033a9a91cf0e79b8c
hikasa_akito <wetsand.wfs@gmail.com>
2020-11-14 16:29:24 +0900

    hoge

---
+++ app/controllers/lendings_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class LendingsController < ApplicationController
+  def new; end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users,     only: %i[index new create]
   resources :machines,  only: %i[new create]
   resources :offices,   only: %i[new create]
+  resources :lendings,  only: %i[new create]
 end
