a13206b50f924ba891d8d181e9b78bc8a8d9fb2c
hikasa_akito <wetsand.wfs@gmail.com>
2020-11-15 15:21:18 +0900

    fix import error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
-require("src/bootstrap/bootstrap")
-require("src/dashboard.css")
-require("src/signin.css")
+import "../styles/bootstrap/bootstrap.sass"
+import "../styles/dashboard.css"
+import "../styles/signin.css"
+
