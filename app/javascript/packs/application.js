54d1780ed413b10ff7e961b5cbc1cb040be9d10b
hikasa_akito <wetsand.wfs@gmail.com>
2020-11-15 09:38:49 +0900

    remove style template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 // require("channels")
-//require("src/sb-admin-2")
 require("src/bootstrap/bootstrap")
 require("src/dashboard.css")
 // Uncomment to copy all static images under ../images to the output folder and reference
