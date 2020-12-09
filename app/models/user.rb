b1f4f20ecb4a9a715d2be5767990294e36bd6b2c
mrbigass <wetsand.wfs@gmail.com>
2020-12-09 23:43:47 +0900

    remove deviese files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable , :confirmable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :trackable
-
   belongs_to :office
 end
