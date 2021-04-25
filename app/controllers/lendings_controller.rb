293cacca85e3684bcc7609ca0052dbe8bf8b524f
mrbigass <wetsand.wfs@gmail.com>
2021-04-25 21:52:18 +0900

    貸し出しの登録

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -3,5 +3,15 @@
 class LendingsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    lending = Lending.new(
+      office: Office.first,
+      customer: Customer.first,
+      machine: Machine.first,
+      from: '2021-04-25 04:17:49 UTC',
+      to: '2021-04-26 04:17:49 UTC'
+    )
+
+    redirect_to root_path if lending.save
+  end
 end
--- app/models/lending.rb
+++ app/models/lending.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 
-class Lending < ApplicationRecord; end
+class Lending < ApplicationRecord
+  belongs_to :office
+  belongs_to :machine
+  belongs_to :customer
+
+  attribute :from, :datetime, default: -> { Time.now }
+  attribute :to, :datetime, default: -> { Time.now + 2.weeks }
+
+  validates :office, presence: true
+  validates :machine, presence: true
+  validates :customer, presence: true
+  validate :not_return_before_lend
+
+  def not_return_before_lend
+    errors.add(:validate, '返却を貸し出し前に設定できません') if to < from
+  end
+end
