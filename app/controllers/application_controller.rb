4d1e4208a204c3b4d2f40d417be300fec6167e63
mrbigass <wetsand.wfs@gmail.com>
2021-05-02 15:04:52 +0900

    ルーティング消す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,43 +2,4 @@
 
 class ApplicationController < ActionController::Base
   include UserAuthentications
-  before_action :check_authentication, only: %i[home]
-
-  def home
-    @offices = Office.all
-    @machines = Machine.all
-    @customers = Customer.all
-    @lendings = Lending.preload(%i[customer machine]).all
-  end
-
-  def registration; end
-
-  def registration_post
-    user = User.new(
-      office: Office.first,
-      name: params[:name],
-      email: params[:email],
-      password: params[:password],
-      password_confirmation: params[:password_confirmation]
-    )
-
-    user.save!
-  end
-
-  def login
-    redirect_to root_path if session[:user_id]
-  end
-
-  def login_authenticate
-    authenticate(email: params[:email], password: params[:password])
-    redirect_to root_path
-  end
-
-  def current_user
-    @current_user ||= check_authentication
-  end
-
-  def current_office
-    @current_office ||= current_user.office
-  end
 end
