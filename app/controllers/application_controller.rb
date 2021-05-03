367cf95ef1b1281b9d0bbd05e35f2cd20765350c
mrbigass <wetsand.wfs@gmail.com>
2021-05-03 10:44:38 +0900

    CSRFの認証をスキップ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@
 
 class ApplicationController < ActionController::Base
   include UserAuthentications
+  # TODO: CSRF対策のトークン検証をスキップしているので落ち着いたら戻す
+  skip_before_action :verify_authenticity_token
 end
