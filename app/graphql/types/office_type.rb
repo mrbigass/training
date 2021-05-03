bc1b816e203044e69c538dfbef5c7dfde550ef87
mrbigass <wetsand.wfs@gmail.com>
2021-05-03 13:33:55 +0900

    wip officeからのリレーションでmachine_typeを追加する

--- app/graphql/types/office_type.rb
+++ app/graphql/types/office_type.rb
@@ -4,6 +4,6 @@ module Types
   class OfficeType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
-    field :machines, [Type::MachineType], null: false
+    # field :machines, [Type::MachineType], null: false
   end
 end
