ac67a957a392d494f6b1ca838ff77c74de1180ce
mrbigass <wetsand.wfs@gmail.com>
2021-05-27 22:28:37 +0900

    rubo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '*', headers: :any, methods: [:get, :post]
+    resource '*', headers: :any, methods: %i[get post]
   end
 end
