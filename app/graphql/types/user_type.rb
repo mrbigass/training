908fadf328a83ad5ba259b2659afb45305e4f3d3
mrbigass <wetsand.wfs@gmail.com>
2021-06-01 21:29:37 +0900

    fieldの追加忘れ

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,6 +4,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :email, String, null: false
     field :office, Types::OfficeType, null: false
   end
 end
