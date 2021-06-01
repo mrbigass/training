cc430bf46916f4122e14219c793296c70cc8310f
mrbigass <wetsand.wfs@gmail.com>
2021-06-01 22:08:12 +0900

    不要行

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,6 @@
 module Types
   class QueryType < Types::BaseObject
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
 
     field :offices, [Types::OfficeType], null: false
     def offices
